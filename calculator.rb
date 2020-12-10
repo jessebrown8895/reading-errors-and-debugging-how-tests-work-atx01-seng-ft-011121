85b02311bbbb211a1379eb31440c637e41ca0949
Jesse Brown <brownjesse8895@yahoo.com>
2020-12-10 20:46:19 +0000

    Automatically backed up by Learn

--- calculator.rb
+++ calculator.rb
@@ -9,6 +9,7 @@ first_number
 sum = first_number + second_number 
  difference = first_number - second_number 
  product = first_number * second_number 
+ quotient = first_number / second_number 
 
 
 
