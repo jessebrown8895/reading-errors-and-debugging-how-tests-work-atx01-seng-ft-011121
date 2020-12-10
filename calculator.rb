ad3d24cf434d6bc70a0e26f2f4e9748518d32c92
Jesse Brown <brownjesse8895@yahoo.com>
2020-12-10 20:41:44 +0000

    Automatically backed up by Learn

--- calculator.rb
+++ calculator.rb
@@ -2,10 +2,11 @@ first_number = 10
 second_number = 5
 def first_number
   puts first_number + second_number
-  sum = first_number + second_number 
 end
 
 first_number
+
+sum = first_number + second_number 
 
 
 
